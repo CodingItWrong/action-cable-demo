3e8c61c21c7eea1c86a13577c64e40339b87ae6f
Josh Justice <me@codingitwrong.com>
2019-06-28 20:22:22 -0400

    Remove log

--- frontend/MessageForm.js
+++ frontend/MessageForm.js
@@ -12,9 +12,9 @@ class MessageForm extends React.Component {
   }
 
   handleSubmit(e) {
+
     e.preventDefault();
     App.cable.subscriptions.subscriptions[0].speak({ message: this.state.body });
-    console.log('sent', this.state.body)
     this.setState({ body: "" });
   }
 
